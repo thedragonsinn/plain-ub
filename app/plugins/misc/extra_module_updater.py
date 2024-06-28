0c35e70ef9381a5f1cdb2e8b1ffeaad459028bf2
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-06-28 19:50:38 +0530

    restart on ext module update

--- app/plugins/misc/extra_module_updater.py
+++ app/plugins/misc/extra_module_updater.py
@@ -1,3 +1,4 @@
+from ub_core.default_plugins.restart import restart
 from ub_core.utils import run_shell_cmd
 
 from app import BOT, Message, bot
@@ -8,4 +9,6 @@ async def social_dl_update(bot: BOT, message: Message):
     output = await run_shell_cmd(
         cmd="cd app/modules && git pull", timeout=10, ret_val=0
     )
-    await message.reply(output)
+    update_notif = await message.reply(output)
+    if output and output != "Already up to date.":
+        await restart(bot, message, update_notif)
--- scripts/install_external_modules.sh
+++ scripts/install_external_modules.sh
@@ -3,7 +3,9 @@ if [ -z "${EXTRA_MODULES_REPO}" ]; then
     exit
 fi
 
-git clone "${EXTRA_MODULES_REPO}" "app/modules"
+echo "Installing External Modules"
+
+git clone -q "${EXTRA_MODULES_REPO}" "app/modules"
 
 pip -q install --no-cache-dir -r req*.txt
 
