a132c063dde6fad7de7cdba5daca5c643e92baab
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-07-31 14:51:45 +0530

    new way to do hard restart

--- app/plugins/misc/extra_module_updater.py
+++ app/plugins/misc/extra_module_updater.py
@@ -7,8 +7,8 @@ from app import BOT, Message, bot
 @bot.add_cmd(cmd="supdate", allow_sudo=False)
 async def social_dl_update(bot: BOT, message: Message):
     output = await run_shell_cmd(
-        cmd="cd app/modules && git pull", timeout=10, ret_val=0
+        cmd="cd app/modules && git pull", timeout=10, ret_val="0"
     )
     update_notif = await message.reply(output)
-    if output and output != "Already up to date.":
+    if output.strip() != "Already up to date.":
         await restart(bot, message, update_notif)
