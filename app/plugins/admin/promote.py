4754f63ba01f9b0ca5bd8b10114a52b02209c86d
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-08-18 20:48:46 +0530

    bugfix: do not assume user has full rights during .promote

--- app/plugins/admin/promote.py
+++ app/plugins/admin/promote.py
@@ -1,36 +1,23 @@
 import asyncio
 
+from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import ChatPrivileges, User
 
 from app import BOT, Message, bot
 
+DEMOTE_PRIVILEGES = ChatPrivileges(can_manage_chat=False)
 
-def get_privileges(
-    anon: bool = False, full: bool = False, without_rights: bool = False
-) -> ChatPrivileges:
-    if without_rights:
-        return ChatPrivileges(
-            can_manage_chat=True,
-            can_manage_video_chats=False,
-            can_pin_messages=False,
-            can_delete_messages=False,
-            can_change_info=False,
-            can_restrict_members=False,
-            can_invite_users=False,
-            can_promote_members=False,
-            is_anonymous=False,
-        )
-    return ChatPrivileges(
-        can_manage_chat=True,
-        can_manage_video_chats=True,
-        can_pin_messages=True,
-        can_delete_messages=True,
-        can_change_info=True,
-        can_restrict_members=True,
-        can_invite_users=True,
-        can_promote_members=full,
-        is_anonymous=anon,
-    )
+NO_PRIVILEGES = ChatPrivileges(
+    can_manage_chat=True,
+    can_manage_video_chats=False,
+    can_pin_messages=False,
+    can_delete_messages=False,
+    can_change_info=False,
+    can_restrict_members=False,
+    can_invite_users=False,
+    can_promote_members=False,
+    is_anonymous=False,
+)
 
 
 @bot.add_cmd(cmd=["promote", "demote"])
@@ -47,34 +34,51 @@ async def promote_or_demote(bot: BOT, message: Message) -> None:
     response: Message = await message.reply(
         f"Trying to {message.cmd.capitalize()}....."
     )
+
+    my_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=bot.me.id)
+    my_privileges = my_status.privileges
+
+    if not (
+        my_status.status in {ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR}
+        and my_privileges.can_promote_members
+    ):
+        await response.edit("You don't to have enough rights to do this.")
+        return
+
     user, title = await message.extract_user_n_reason()
+
     if not isinstance(user, User):
         await response.edit(user, del_in=10)
         return
-    full: bool = "-full" in message.flags
-    anon: bool = "-anon" in message.flags
-    without_rights = "-wr" in message.flags
+
+    my_privileges.can_promote_members = "-full" in message.flags
+    my_privileges.is_anonymous = "-anon" in message.flags
+
     promote = message.cmd == "promote"
+
     if promote:
-        privileges: ChatPrivileges = get_privileges(
-            full=full, anon=anon, without_rights=without_rights
-        )
+        final_privileges = NO_PRIVILEGES if "-wr" in message.flags else my_privileges
     else:
-        privileges = ChatPrivileges(can_manage_chat=False)
+        final_privileges = DEMOTE_PRIVILEGES
+
     response_text = f"{message.cmd.capitalize()}d: {user.mention}"
+
     try:
         await bot.promote_chat_member(
-            chat_id=message.chat.id, user_id=user.id, privileges=privileges
+            chat_id=message.chat.id, user_id=user.id, privileges=final_privileges
         )
+
         if promote:
             await asyncio.sleep(1)
             await bot.set_administrator_title(
                 chat_id=message.chat.id, user_id=user.id, title=title or "Admin"
             )
+
             if title:
                 response_text += f"\nTitle: {title}"
             if without_rights:
                 response_text += "\nWithout Rights: True"
+
         await response.edit(text=response_text)
     except Exception as e:
         await response.edit(text=e, del_in=10, block=True)
