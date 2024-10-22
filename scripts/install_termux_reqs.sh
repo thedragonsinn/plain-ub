83c6c753ccbae230c1d9bcd9f341e7f7a87b1623
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-10-22 17:38:20 +0530

    `scripts`: drop handling core reqs on termux

--- scripts/install_termux_reqs.sh
+++ scripts/install_termux_reqs.sh
@@ -5,10 +5,6 @@ if ! echo "${PATH}" | grep -qi "com.termux"; then
   exit
 fi
 
-ub_core_req_url="https://github.com/thedragonsinn/ub-core/raw/main/requirements.txt"
-
-curl -fsSL ${ub_core_req_url} | grep -Ev "uvloop|psutil" | xargs pip install 
-
-./scripts/install_ub_core.sh "--no-deps"
+./scripts/install_ub_core.sh
 
 grep -Ev "google-generativeai|pillow" req.txt | xargs -n 1 pip install
--- scripts/install_ub_core.sh
+++ scripts/install_ub_core.sh
@@ -6,4 +6,4 @@ if [ ! -z "${USE_DUAL_BRANCH}" ]; then
     dual_mode_arg="@dual_mode"
 fi
 
-pip -q install --no-cache-dir --force-reinstall $1 "git+https://github.com/thedragonsinn/ub-core${dual_mode_arg}"
+pip -q install --no-cache-dir --force-reinstall "git+https://github.com/thedragonsinn/ub-core${dual_mode_arg}"
