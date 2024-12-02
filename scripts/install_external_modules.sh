03bafd9c6776cc8d2693061f2aa7bb65e0b7a94e
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-12-02 18:14:03 +0530

    install external modules.sh now shows repo name while installing.

--- scripts/install_external_modules.sh
+++ scripts/install_external_modules.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 if [ -z "${EXTRA_MODULES_REPO}" ]; then
+    echo "EXTRA_MODULES_REPO not set, Skipping..."
     exit
 fi
 
-echo "Installing External Modules"
+repo_name=$(basename "${EXTRA_MODULES_REPO}")
+
+echo "Installing ${repo_name} to app/modules"
 
 git clone -q "${EXTRA_MODULES_REPO}" "app/modules"
 
 pip -q install --no-cache-dir -r app/modules/req*.txt
 
+echo "Done"
 
