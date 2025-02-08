b0e1e3b68cb289ff9322c62c2927a5dbd4c348b7
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2025-02-08 12:00:25 +0530

    supress mkdir error

--- scripts/install_termux_reqs.sh
+++ scripts/install_termux_reqs.sh
@@ -2,7 +2,7 @@
 
 grep -qi "com.termux" <<< "$PATH" || { echo "Not a termux Env, Skipping..."; exit; }
 
-mkdir -p "${HOME}/.config/pip" /dev/null 2>&1
+mkdir -p "${HOME}/.config/pip" > /dev/null 2>&1
 
 echo -e '[global]\nextra-index-url = https://termux-user-repository.github.io/pypi/' > "${HOME}/.config/pip.conf"
 
