4772d824ce8da2a597831c29dc9d62ce74c55cef
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2025-02-18 16:53:27 +0530

    `song`: clean code and add inline support.

--- app/plugins/misc/song.py
+++ app/plugins/misc/song.py
@@ -1,12 +1,13 @@
 import asyncio
-import glob
 import json
-import os
 import shutil
+from pathlib import Path
 from time import time
 from urllib.parse import urlparse
 
-from ub_core.utils import MediaExts, aio, run_shell_cmd
+from pyrogram.enums import MessageEntityType
+from pyrogram.types import InputMediaAudio
+from ub_core.utils import aio, run_shell_cmd
 
 from app import BOT, Message
 
@@ -20,16 +21,28 @@ domains = [
 ]
 
 
+def is_yt_url(url: str) -> bool:
+    return urlparse(url).netloc in domains
+
+
+def extract_link_from_reply(message: Message) -> str | None:
+    if not message:
+        return
+
+    for link in message.text_list:
+        if is_yt_url(link):
+            return link
+
+    for entity in message.entities or []:
+        if entity.type == MessageEntityType.TEXT_LINK and is_yt_url(entity.url):
+            return entity.url
+
+    return None
+
+
 @BOT.add_cmd(cmd="song")
 async def song_dl(bot: BOT, message: Message) -> None | Message:
-    reply_query = None
-
-    for link in message.reply_text_list:
-        if urlparse(link).netloc in domains:
-            reply_query = link
-            break
-
-    query = reply_query or message.filtered_input
+    query = extract_link_from_reply(message.replied) or message.filtered_input
 
     if not query:
         await message.reply("Give a song name or link to download.")
@@ -37,44 +50,40 @@ async def song_dl(bot: BOT, message: Message) -> None | Message:
 
     response: Message = await message.reply("Searching....")
 
-    download_path: str = os.path.join("downloads", str(time()))
+    download_path: str = Path("downloads") / str(time())
 
     query_or_search: str = query if query.startswith("http") else f"ytsearch:{query}"
 
     song_info: dict = await get_download_info(query=query_or_search, path=download_path)
 
-    duration, artist, thumb = 0, "", None
+    audio_files: list = list(download_path.glob("*mp3"))
 
-    if isinstance(song_info, dict):
-        duration: int = song_info["duration"]
-        artist: str = song_info["channel"]
-        thumb = await aio.in_memory_dl(song_info["thumbnail"])
-
-    down_path: list = glob.glob(os.path.join(download_path, "*"))
-
-    if not down_path:
+    if not audio_files:
         await response.edit("Song Not found.")
         return
 
-    await response.edit("Uploading....")
+    audio_file = audio_files[0]
 
-    for audio_file in down_path:
-        if audio_file.endswith(tuple(MediaExts.AUDIO)):
-            await message.reply_audio(
-                audio=audio_file,
-                duration=int(duration),
-                performer=str(artist),
-                thumb=thumb,
-            )
+    url = song_info.get("webpage_url")
 
-    await response.delete()
+    await response.edit(f"`Uploading {audio_file.name}....`")
+
+    await response.edit_media(
+        InputMediaAudio(
+            media=str(audio_file),
+            caption=f"<a href={url}>{audio_file.name}</a>" if url else None,
+            duration=int(song_info.get("duration", 0)),
+            performer=song_info.get("channel", ""),
+            thumb=await aio.in_memory_dl(song_info.get("thumbnail")),
+        )
+    )
 
     shutil.rmtree(download_path, ignore_errors=True)
 
 
-async def get_download_info(query: str, path: str):
+async def get_download_info(query: str, path: Path) -> dict:
     download_cmd = (
-        f"yt-dlp -o '{os.path.join(path, '%(title)s.%(ext)s')}' "
+        f"yt-dlp -o '{path/'%(title)s.%(ext)s'}' "
         f"-f 'bestaudio' "
         f"--no-warnings "
         f"--ignore-errors "
@@ -82,14 +91,16 @@ async def get_download_info(query: str, path: str):
         f"--quiet "
         f"--no-playlist "
         f"--audio-quality 0 "
+        f"--audio-format mp3 "
         f"--extract-audio "
         f"--embed-thumbnail "
         f"--embed-metadata "
         f"--print-json "
         f"'{query}'"
     )
+
     try:
-        song_info = (await run_shell_cmd(download_cmd)).strip()
+        song_info = (await run_shell_cmd(download_cmd, timeout=60, ret_val="")).strip()
 
         serialised_json = json.loads(song_info)
         return serialised_json
@@ -98,4 +109,6 @@ async def get_download_info(query: str, path: str):
         shutil.rmtree(path=path, ignore_errors=True)
 
     except json.JSONDecodeError:
-        return
+        pass
+
+    return {}
