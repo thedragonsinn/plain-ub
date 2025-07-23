8a94b6d4d1e4fc7eb80e790cd6ad52abb61ab8a6
Silas Alves <87999401+hastfrin@users.noreply.github.com>
2025-07-23 19:30:02 -0300

    traduzir: app/__main__.py para PT‑BR

--- app/__main__.py
+++ app/__main__.py
@@ -3,11 +3,12 @@ import sys
 from app import LOGGER, Config, bot
 
 if Config.CMD_TRIGGER == Config.SUDO_TRIGGER:
-    LOGGER.error("CMD_TRIGGER and SUDO_TRIGGER can't be the same")
+    LOGGER.error("CMD_TRIGGER e SUDO_TRIGGER não podem ser iguais")
     sys.exit(1)
 
 
 if __name__ == "__main__":
     bot.run(bot.boot())
 else:
-    LOGGER.error("Wrong Start Command.\nUse 'python -m app'")
+    LOGGER.error("Comando de inicialização errado.\nUse 'python -m app'")
+
